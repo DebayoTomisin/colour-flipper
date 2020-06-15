0689e1a8a60365deb3e61d32251c957c09cee9ed
Debayo Tomisin <debayotomisin07@gmail.com>
2020-06-15 11:47:19 -0700

    final commit

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const button = document.querySelector('.btn');
+const button = document.querySelector('.btn')
 let container = document.querySelector('.container');
 
-button.addEventListener('click', changeColor)
+button.addEventListener('click', changeColor);
 
 function changeColor() {
-
     let x = Math.round(Math.random() * 256);
     let y = Math.round(Math.random() * 256);
     let z = Math.round(Math.random() * 256);
 
-    let color = `rgb(${x}, ${y}, ${z})`;
+    let color = `rgba(${x}, ${y}, ${z}, .3)`
     container.style.backgroundColor = color;
 }
